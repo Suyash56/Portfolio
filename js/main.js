85d88dd8140ef77f74647c97cbcaa3276b768ff1
Suyash56 <suyashbalshetwar5@gmail.com>
2020-07-29 15:26:06 +0530

    Add all my files

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// jQuery(document).ready(function(){
+//     $('.text').typed({
+//           strings: ["We Are <strong class='primary'>SPARKY.</strong>", "We Are <strong class='secondary'>Awesome Developers.</strong>"],
+//           typeSpeed: 0,
+//           loop:true
+//       });
+//  });
+
+ $(function(){
+	$(".typed").typed({
+		strings: ["Developers.", "Designers.", "People."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
